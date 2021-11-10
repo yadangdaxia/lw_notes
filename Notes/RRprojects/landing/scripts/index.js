9bf10325cb495321109260daeaed85469fc7757c
yadangdaxia <matteson211@hotmail.com>
2021-11-10 09:52:40 +0800

    added js form

--- Notes/RRprojects/landing/scripts/index.js
+++ Notes/RRprojects/landing/scripts/index.js
@@ -1,30 +1,9 @@
-// Variables
-const skateboard = document.querySelector('#skateboard');
-const flower = document.querySelector('#flower');
-const pony = document.querySelector('#pony');
-const heroButton = document.querySelector('#hero-button');
 
-// Hero buttons
+// HERO
+
+const heroButton = document.querySelector('#hero-button');
 const unclicked = "Kick ass"
 const clicked = "Yeah!!"
-
-// Change skateboard from square to circle
-skateboard.addEventListener('click', () => {  // can add the word event in the parentheses, or event.currentTarget to get info in browser
-  //console.log("You clicked the image!");
-  skateboard.classList.toggle('img-circle');  // change add to toggle to have it go back after clicking
-});
-
-flower.addEventListener('click', () => {
-  console.log("you clicked the flower")
-  flower.classList.toggle('img-darken');
-});
-
-pony.addEventListener('click', (e) => {
-  console.log(e)
-  pony.classList.toggle('img-shrink');
-  let sound = new Audio('images/horse_noise.mp3')
-  sound.play();
-});
 
 heroButton.addEventListener('click', (e) => {
   console.log(e)
@@ -43,7 +22,38 @@ heroButton.addEventListener('click', () => {
   newButton.classList.remove('#hidden-button')
 });
 
-// Functions from buttons.js 
+
+
+// CLICKABLE JS EVENTS
+
+const skateboard = document.querySelector('#skateboard');
+const flower = document.querySelector('#flower');
+const pony = document.querySelector('#pony');
+
+// Change skateboard from square to circle
+skateboard.addEventListener('click', () => {  // can add the word event in the parentheses, or event.currentTarget to get info in browser
+  //console.log("You clicked the image!");
+  skateboard.classList.toggle('img-circle');  // change add to toggle to have it go back after clicking
+});
+
+// Change tint of flower
+flower.addEventListener('click', () => {
+  console.log("you clicked the flower")
+  flower.classList.toggle('img-darken');
+});
+
+// Make pony shrink and play sound
+pony.addEventListener('click', (e) => {
+  console.log(e)
+  pony.classList.toggle('img-shrink');
+  let sound = new Audio('images/horse_noise.mp3')
+  sound.play();
+});
+
+
+
+// SELECTABLE BUTTONS
+
 const selectAllButtons = () => {
   const reset = document.querySelector('#select-all')
   // console.log(reset);
@@ -78,8 +88,18 @@ const selectMultipleButtons = () => {
   });
 }
 
-
 selectAllButtons();
 clearAllButtons();
 selectMultipleButtons();
 
+
+
+
+// DOM MANIPULATION
+
+// Look at all the properties in the document
+console.dir(document);
+// Change the title
+// document.title = "123"
+// console.log(document.all);
+// document.all[97].textContent = "hello"
