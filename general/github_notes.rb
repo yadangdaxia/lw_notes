ac0b410b9c18b1d7103ec6cb8b6edb61099af0a8
Adam Matteson <matteson211@hotmail.com>
2021-12-09 19:18:52 +0800

    updated?

--- general/github_notes.rb
+++ general/github_notes.rb
@@ -3,19 +3,13 @@
 git init
 git add .
 git commit -m "my profile page"
-gh repo create # this creates the associated repo on Github! Can actually commit before creating this and files will stay "committed" 
+gh repo create # this creates the associated repo on Github! Can actually commit before creating this and files will stay "committed"
+
+
 
 # To open the Github repo from your browser you can run:
-
 gh repo view --web
 
-# Github Pages is a sub-service of Github that makes it easy
-# to deploy any static website in 10 seconds (static == not a Rails app).
-# It is based on a “magic” branch, called gh-pages. When Github detects
-# this branch, it puts your website online.
-
-git co -b gh-pages
-git push origin gh-pages # we push the gh-pages branch, not master!
 
 
 # Your profile can be pushed on http://yadangdaxia.github.io/profile/ through usual git commands:
@@ -24,18 +18,29 @@ git add .
 git commit -m "make my profile prettier"
 git push origin gh-pages
 
-token: (use when updating to main)
-git add . 
+
+#token: (use when updating to main)
+git add .
 git commit -m "xx"
 git push origin main
 
-Q: 
-How to not have to generate new token each time I push from one computer? 
+# Q: How to not have to generate new token each time I push from one computer?
 https://github.com/settings/tokens
+# Need to give your public key to github.
 
-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
+# See setup instructions under GitHub CLI and
+https://github.com/lewagon/setup
 
 
 
 
 
+# GITHUB PAGES
+
+# Github Pages is a sub-service of Github that makes it easy
+# to deploy any static website in 10 seconds (static == not a Rails app).
+# It is based on a “magic” branch, called gh-pages. When Github detects
+# this branch, it puts your website online.
+
+git co -b gh-pages
+git push origin gh-pages # we push the gh-pages branch, not master!
